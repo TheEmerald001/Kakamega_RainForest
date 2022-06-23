5326a2d7a52abe24a510b196a7aac6bfcf770a22
steve <stephen.lukanu@student.moringaschool.com>
2022-06-23 21:39:15 +0300

    adds preliminary script with animal load capability

---
+++ index.js
@@ -0,0 +1,21 @@
+function renderTrail(animal){
+    let show = document.createElement('div')  // Append comes here//
+    show.className = "oneSpecies" //we would style this with css
+    show.innerHTML = `
+    <img src="${animal.image}" alt="">
+    <h5 class="speciesName"> ${animal.name}</h5>
+    <p class="specieDescription">${animal.desc}</p>
+</div>
+    ` //this is how I would add a single animal
+    document.querySelector('#allSpecies').appendChild(show)
+}
+//fetch trail data
+function getTrailData(){
+    fetch('http://localhost:3000/animals')
+    .then(response => response.json())
+    .then(trailData => trailData.forEach(animal => renderTrail(animal)));
+}
+function showAnimal(){
+    getTrailData()
+}
+showAnimal();
