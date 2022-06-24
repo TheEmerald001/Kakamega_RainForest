04281aa1060b7ae8b58279fe32ff1def0d86ffe1
steve <stephen.lukanu@student.moringaschool.com>
2022-06-24 21:16:57 +0300

    adds script to listen and act on given trail input

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-function renderTrail(animal){
-    let show = document.createElement('div')  // Append comes here//
-    show.className = "oneSpecies" //we would style this with css
+//If I want to capture the radio button input by the tourist;
+const trailOn = function(){
+    const start = document.getElementsByName('startTrail'); //an array of all radios named trails
+    console.log(start);
+    for (i = 0; i < start.length; i++){
+        if(start[i].checked){ 
+            console.log(start[i].value);
+            return  start[i].value; //
+        }
+    }
+  }
+//console.log(`${trailOn()}`);
+function renderTrail(trailOn){
+    let show = document.createElement('div');  // Append comes here//
+    show.className = "oneSpecies"; //we will style this with css
     show.innerHTML = `
-    <img src="${animal.image}" alt="">
-    <h5 class="speciesName"> ${animal.name}</h5>
-    <p class="specieDescription">${animal.desc}</p>
+    <img src="${trailOn.image}" alt="">
+    <h5 class="speciesName"> ${trailOn.name}</h5>
+    <p class="specieDescription">${trailOn.desc}</p>
     <div class="withBtn">
         <button class="submitBtn"> Donate </button>
     </div>
 </div>
-    ` //this is how I would add a single animal
-    document.querySelector('#allSpecies').appendChild(show)
+    `; //this is how I  add a single animal
+    document.querySelector('#allSpecies').appendChild(show);
 }
-//fetch trail data
-function getTrailData(){
-    fetch('http://localhost:3000/animals')
-    .then(response => response.json())
-    .then(trailData => trailData.forEach(animal => renderTrail(animal)));
-}
-function showAnimal(){
-    getTrailData()
-}
-showAnimal();
+const pageAction = () => 
+    {
+        const inputForm = document.querySelector('form');
+        inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let trail = trailOn();
+        //fetch trail datagit
+        fetch(`http://localhost:3000/${trail}`)
+        .then(response => response.json())
+        .then(trailData => trailData.forEach(trailOn => renderTrail(trailOn)));
+    });
+  };
+   document.addEventListener('DOMContentLoaded', pageAction);
